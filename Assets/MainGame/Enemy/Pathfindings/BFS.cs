2e169c700c49ea29f749eb0a3e15422beecf5ac5
darkstryller <83669103+darkstryller@users.noreply.github.com>
2025-05-24 21:48:47 -0300

    path finding

---
+++ Assets/MainGame/Enemy/Pathfindings/BFS.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class BFS
+{
+    public static List<T> Run<T>(T start, Func<T, bool> isSatisfied, Func<T, List<T>> getConnections, int watchdog = 500, int watchdogPath = 500)
+    {
+        Dictionary<T, T> parents = new Dictionary<T, T>();
+        Queue<T> pending = new Queue<T>();
+        HashSet<T> visited = new HashSet<T>();
+
+        pending.Enqueue(start);
+        while (pending.Count > 0)
+        {
+            watchdog--;
+            if (watchdog <= 0) break;
+            T current = pending.Dequeue();
+            Debug.Log("BFS");
+            if (isSatisfied(current))
+            {
+                List<T> path = new List<T>();
+                path.Add(current);
+                while (parents.ContainsKey(path[path.Count - 1]))
+                {
+                    watchdogPath--;
+                    if (watchdogPath <= 0) break;
+                    path.Add(parents[path[path.Count - 1]]);
+                }
+                path.Reverse();
+                return path;
+            }
+            else
+            {
+                visited.Add(current);
+                List<T> connections = getConnections(current);
+
+                for (int i = 0; i < connections.Count; i++)
+                {
+                    T child = connections[i];
+                    if (visited.Contains(child) || pending.Contains(child)) continue;
+                    pending.Enqueue(child);
+                    parents[child] = current;
+                }
+            }
+        }
+
+        return new List<T>();
+    }
+}
